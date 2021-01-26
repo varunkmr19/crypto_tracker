740f1c21ba2a8b19bc846456ca1a85d1a36472ab
varunkmr19 <varun-kumar@live.com>
2021-01-26 07:59:16 +0530

    add templates

--- positions/views.py
+++ positions/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import requests
 
 # Create your views here.
@@ -10,4 +9,6 @@ def index_view(request):
     URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     data = requests.get(URL).json()
 
-    return HttpResponse(data)
+    context = {'data': data}
+
+    return render(request, 'positions/index.html', context)
