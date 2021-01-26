b2dee1b4f3daa3ca57b85e531c1c86b6420df27c
varunkmr19 <varun-kumar@live.com>
2021-01-26 07:32:15 +0530

    add index view

--- positions/views.py
+++ positions/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
 
 # Create your views here.
+
+
+def index_view(request):
+    # API Endpoint
+    URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    data = requests.get(URL).json()
+
+    return HttpResponse(data)
